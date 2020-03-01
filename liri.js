160bc51436ae932c0761d61229f022e9d4dfd3a9
Justin Acosta <leopoldacosta@gmail.com>
2020-03-01 14:32:16 -0500

    udpated

--- liri.js
+++ liri.js
@@ -1,15 +1,18 @@
-// require(".env").config();
+
+// var env = require(".env").config();
 // var require
-var keys = require("./keys.js");
+// var keys = require("./keys.js");
 // var spotify = new Spotify(keys.spotify);
 var fs = require("fs");
-
+var axios = require("axios");
 var appSelection = process.argv[2];
 var userChoice = process.argv[3];
 
+// if userChoice extends one word
 for (var i = 4; i < process.argv.length; i++) {
-    userChoice += "+"+ process.argv[i];
+    userChoice += "+" + process.argv[i];
 };
+
 
 //present directions
 // console.log("Welcome to LIRI BOT! To utilize effectively, please follow the follow syntax!");
@@ -26,12 +29,69 @@ switch (appSelection) {
         break;
     case "spotify-this-song":
         console.log(userChoice);
-        break; 
+        break;
     case "movie-this":
         console.log(userChoice);
+        getMovie();
         break;
 };
 
-//function that appends search information to txt file, include function inside of switch
 
-//function that connects userinput to axios get info to shorten code
+
+//function fetch movie information
+function getMovie() {
+    //function that appends search information to txt file, include function inside of switch
+    axios.get("http://www.omdbapi.com/?t=" + userChoice + "&y=&plot=short&apikey=trilogy").then(
+        function (response) {
+            console.log("The movie's rating is: " + response.data.imdbRating);
+        })
+        .catch(function (error) {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log("---------------Data---------------");
+                console.log(error.response.data);
+                console.log("---------------Status---------------");
+                console.log(error.response.status);
+                console.log("---------------Status---------------");
+                console.log(error.response.headers);
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
+    //function that connects userinput to axios get info to shorten code
+}
+
+// function to get bands
+function getBands() {
+    axios.get("https://rest.bandsintown.com/artists/" + userChoice + "/events?app_id=codingbootcamp").then(
+        function (response) {
+            console.log("The movie's rating is: " + response.data.imdbRating);
+        })
+        .catch(function (error) {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log("---------------Data---------------");
+                console.log(error.response.data);
+                console.log("---------------Status---------------");
+                console.log(error.response.status);
+                console.log("---------------Status---------------");
+                console.log(error.response.headers);
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
+}
